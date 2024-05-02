f7ecb4e3301f48e2f9f0789390c5a0dec24f6b7a
MoraisGustavo1 <gmoraisildefonso@gmail.com>
2024-05-02 11:03:36 -0300

    Criando a função de adição de tarefas

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,18 @@ function validaCampo()
         valida = true;
         return valida;
 }
+
+//Função de adição de tarefas
+
+function adicionarTarefa(){
+    let linhas = document.getElementById('task');
+    if(validaCampo){
+        alert("Preencha o campo com uma tarefa")
+    }else{
+        //Adicionando tarefas
+        tarefa.push(linhas.value);
+        linhas.value="";
+        listarTarefas();
+    }
+    document.getElementById('task').focus();
+}
