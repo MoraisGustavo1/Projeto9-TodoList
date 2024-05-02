4d67d7f63603520ec83b23e157ae174f56bd387e
MoraisGustavo1 <gmoraisildefonso@gmail.com>
2024-05-02 11:22:20 -0300

    Criando função de remoção de tarefas

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,13 @@
 //Declarando as variaveis
 
-let tarefa = [];
+let tarefas = [];
 
 //Função de validação de campo vazio
 
 function validaCampo()
 {
     let valida = false;
-    if(document.getElementById('task').value == "")
+    if(document.getElementById("task").value == "")
         valida = true;
         return valida;
 }
@@ -15,22 +15,29 @@ function validaCampo()
 //Função de adição de tarefas
 
 function adicionarTarefa(){
-    let linhas = document.getElementById('task');
-    if(validaCampo){
+    let linhas = document.getElementById("task");
+    if(validaCampo()){
         alert("Preencha o campo com uma tarefa")
     }else{
         //Adicionando tarefas
-        tarefa.push(linhas.value);
+        tarefas.push(linhas.value);
         linhas.value="";
         listarTarefas();
     }
-    document.getElementById('task').focus();
+    document.getElementById("task").focus();
 }
 
+//Função de listagem de tarefas
 function listarTarefas(){
     let valor="";
-    for(let i=0; i<tarefas.length; i++){
+    for(let i=0; i<tarefas.length;i++){
         valor += tarefas[i] +"<br>";
     }
     document.getElementById("lista").innerHTML =valor;
+}
+
+//Função de remoção de tarefas
+function removerTarefa(){
+    tarefas.pop();
+    listarTarefas();
 }
